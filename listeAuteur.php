78b8696477fc79f1c0bd5bef4e17eaaff711e4a2
slemaire <slemaire@E117-03.CDF36.local>
2017-01-25 16:52:21 +0100

    ajout dans listeAuteur

--- listeAuteur.php
+++ listeAuteur.php
@@ -10,17 +10,29 @@
             require_once 'include/executeRequete.php';
             $cnx=connexion(UTILISATEUR,MOTDEPASSE,SERVER,BASEDEDONNEES);
             echo "<h2>Les auteurs du XVIII eme siecle</h2>";
-            $varSql = "SELECT nom,prenom FROM auteur";
+            $varSql = "SELECT id_auteur,nom,prenom FROM auteur";
             $idRequete = executeRequete($cnx,$varSql);
             while($ligne=$idRequete->fetch()){
-                echo $ligne['nom'].' '.$ligne['prenom'].'-'.'<br>';
+                
+                echo $ligne['nom'].' '.$ligne['prenom'];
+                echo '<form method="post" action="formConsul.php">';
+                echo '<input type="hidden" name="identifiant" value=""'.$ligne['id_auteur'].'"'.'>'.'<input type="submit" value="C"/>'.'</form>';
+                echo '<form method="post" action="formModif.php">';
+                echo '<input type="hidden" name="identifiant" value=""'.$ligne['id_auteur'].'"'.'>'.'<input type="submit" value="M"/>'.'</form>';
+                echo '<form method="post" action="formSup.php">';
+                echo '<input type="hidden" name="identifiant" value=""'.$ligne['id_auteur'].'"'.'>'.'<input type="submit" value="S"/>'.'</form>';
+                
+                
             }
             ?>
             <form method="post" action="formAjout.php">
-    <p>
-        <label>Ajouter un auteur</label></br>
-        <input type="submit" value="Ajouter" />
-    </p>
-</form>
+                <p>
+                <label>Ajouter un auteur</label></br>
+                <input type="submit" value="Ajouter" />
+                
+                
+                
+                </p>
+            </form>
         </body>
 </html>
